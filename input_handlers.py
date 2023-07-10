b466471df25a02a74a3a92834900228171be7288
Randon K. Jokela <randon.k.jokela@boeing.com>
2023-07-10 13:51:34 -0700

    Allow diagonal movement

--- input_handlers.py
+++ input_handlers.py
@@ -4,6 +4,8 @@ import tcod.event
 
 from actions import Action, EscapeAction, MovementAction
 
+# TODO: Put keybindings in a settings file for customizing. Later, allow them to be changed from in-game.
+
 class EventHandler(tcod.event.EventDispatch[Action]):
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
@@ -13,14 +15,28 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
         key = event.sym
 
-        if key == tcod.event.KeySym.UP:
+        if key == tcod.event.KeySym.UP or key == tcod.event.KeySym.KP_8:
             action = MovementAction(dx=0, dy=-1)
-        elif key == tcod.event.KeySym.DOWN:
+        elif key == tcod.event.KeySym.DOWN or key == tcod.event.KeySym.KP_2:
             action = MovementAction(dx=0, dy=1)
-        elif key == tcod.event.KeySym.LEFT:
+        elif key == tcod.event.KeySym.LEFT or key == tcod.event.KeySym.KP_4:
             action = MovementAction(dx=-1, dy=0)
-        elif key == tcod.event.KeySym.RIGHT:
+        elif key == tcod.event.KeySym.RIGHT or key == tcod.event.KeySym.KP_6:
             action = MovementAction(dx=1, dy=0)
+
+        # Diagonal Movements
+        elif key == tcod.event.KeySym.KP_7:         # UPLEFT
+            action = MovementAction(dx=-1, dy=-1)
+        elif key == tcod.event.KeySym.KP_9:         # UPRIGHT
+            action = MovementAction(dx=1, dy=-1)
+        elif key == tcod.event.KeySym.KP_1:         # DOWNLEFT
+            action = MovementAction(dx=-1, dy=1)
+        elif key == tcod.event.KeySym.KP_3:         # DOWNRIGHT
+            action = MovementAction(dx=1, dy=1)
+
+        # Wait in place
+        # elif key == tcod.event.KeySym.KP_5:
+        #     action = TODO_WaitAction()
 
         elif key == tcod.event.KeySym.ESCAPE:
             action = EscapeAction()
