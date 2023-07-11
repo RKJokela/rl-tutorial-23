2e34b8ef8bebfce3ec4d74e76491ab4d5fd61d09
Randon K. Jokela <randon.k.jokela@boeing.com>
2023-07-11 14:17:41 -0700

    Create Entity and Engine classes

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-from calendar import c
 import tcod
 
 from actions import EscapeAction, MovementAction
+from entity import Entity
 from input_handlers import EventHandler
+from engine import Engine
+
+def draw_entity(console:tcod.console.Console, entity:Entity):
+    console.print(x=entity.x, y=entity.y, string=entity.char, fg = entity.color)
 
 def main() -> None:
-    print('Hello, RL Dev!')
+    tcod_ver = tcod.__version__
+    print('Hello, RL Dev! Using tcod version ' + tcod_ver)
 
     screen_width = 80
     screen_height = 50
     game_title = 'Yet Another Roguelike Tutorial'
-
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
 
     tileset = tcod.tileset.load_tilesheet(
         'dejavu10x10_gs_tc.png', 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
     event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    engine = Engine(entities, event_handler, player)
 
     with tcod.context.new_terminal(
         screen_width,
@@ -29,24 +37,11 @@ def main() -> None:
     ) as context:
         root_console = tcod.console.Console(screen_width, screen_height, order='F')
         while True:
-            root_console.print(x=player_x, y=player_y, string='@')
 
-            context.present(root_console)
+            engine.render(root_console, context)
 
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            events = tcod.event.wait()            
+            engine.handle_events(events)
 
 if __name__ == '__main__':
     main()
