3627d7088c06ab85b7ce75e59cd2088be8c7be4f
Randy Jokela <randyjokela@gmail.com>
2023-07-18 07:26:16 -0700

    Change wall and floor colors

--- src/tile_types.py
+++ src/tile_types.py
@@ -31,8 +31,8 @@ def new_tile(
 
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150))
+    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (75, 75, 150))
 )
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100))
+    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 0))
 )
