1d751b87fad27b8fe40457aa4abb17ec15ac050e
Randy Jokela <randyjokela@gmail.com>
2023-07-28 10:00:42 -0700

    Reorganize input_handlers.py
    Get rid of the long elif chain checking for each possible keypress.

--- src/input_handlers.py
+++ src/input_handlers.py
@@ -4,12 +4,48 @@ from typing import Optional, TYPE_CHECKING
 
 import tcod.event
 
-from actions import Action, EscapeAction, BumpAction
+from actions import Action, EscapeAction, BumpAction, WaitAction
 
 if TYPE_CHECKING:
     from engine import Engine
 
 # TODO: Put keybindings in a settings file for customizing. Later, allow them to be changed from in-game.
+
+MOVE_KEYS = {
+    # Arrow keys.
+    tcod.event.KeySym.UP: (0, -1),
+    tcod.event.KeySym.DOWN: (0, 1),
+    tcod.event.KeySym.LEFT: (-1, 0),
+    tcod.event.KeySym.RIGHT: (1, 0),
+    tcod.event.KeySym.HOME: (-1, -1),
+    tcod.event.KeySym.END: (-1, 1),
+    tcod.event.KeySym.PAGEUP: (1, -1),
+    tcod.event.KeySym.PAGEDOWN: (1, 1),
+    # Numpad keys.
+    tcod.event.KeySym.KP_1: (-1, 1),
+    tcod.event.KeySym.KP_2: (0, 1),
+    tcod.event.KeySym.KP_3: (1, 1),
+    tcod.event.KeySym.KP_4: (-1, 0),
+    tcod.event.KeySym.KP_6: (1, 0),
+    tcod.event.KeySym.KP_7: (-1, -1),
+    tcod.event.KeySym.KP_8: (0, -1),
+    tcod.event.KeySym.KP_9: (1, -1),
+    # Vi keys.
+    tcod.event.KeySym.h: (-1, 0),
+    tcod.event.KeySym.j: (0, 1),
+    tcod.event.KeySym.k: (0, -1),
+    tcod.event.KeySym.l: (1, 0),
+    tcod.event.KeySym.y: (-1, -1),
+    tcod.event.KeySym.u: (1, -1),
+    tcod.event.KeySym.b: (-1, 1),
+    tcod.event.KeySym.n: (1, 1),
+}
+
+WAIT_KEYS = {
+    tcod.event.KeySym.PERIOD,
+    tcod.event.KeySym.KP_5,
+    tcod.event.KeySym.CLEAR,
+}
 
 class EventHandler(tcod.event.EventDispatch[Action]):
     def __init__(self, engine: Engine) -> None:
@@ -37,28 +73,12 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
         player = self.engine.player
 
-        if key == tcod.event.KeySym.UP or key == tcod.event.KeySym.KP_8:
-            action = BumpAction(player, dx=0, dy=-1)
-        elif key == tcod.event.KeySym.DOWN or key == tcod.event.KeySym.KP_2:
-            action = BumpAction(player, dx=0, dy=1)
-        elif key == tcod.event.KeySym.LEFT or key == tcod.event.KeySym.KP_4:
-            action = BumpAction(player, dx=-1, dy=0)
-        elif key == tcod.event.KeySym.RIGHT or key == tcod.event.KeySym.KP_6:
-            action = BumpAction(player, dx=1, dy=0)
+        if key in MOVE_KEYS:
+            dx, dy = MOVE_KEYS[key]
+            action = BumpAction(player, dx, dy)
 
-        # Diagonal Movements
-        elif key == tcod.event.KeySym.KP_7:         # UPLEFT
-            action = BumpAction(player, dx=-1, dy=-1)
-        elif key == tcod.event.KeySym.KP_9:         # UPRIGHT
-            action = BumpAction(player, dx=1, dy=-1)
-        elif key == tcod.event.KeySym.KP_1:         # DOWNLEFT
-            action = BumpAction(player, dx=-1, dy=1)
-        elif key == tcod.event.KeySym.KP_3:         # DOWNRIGHT
-            action = BumpAction(player, dx=1, dy=1)
-
-        # Wait in place
-        # elif key == tcod.event.KeySym.KP_5:
-        #     action = TODO_WaitAction()
+        elif key in WAIT_KEYS:
+            action = WaitAction(player)
 
         elif key == tcod.event.KeySym.ESCAPE:
             action = EscapeAction(player)
