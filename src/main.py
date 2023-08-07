9a3ce98c9b2963ffaf7eba6f1e2c029e3d0a0e26
Randy Jokela <randyjokela@gmail.com>
2023-08-07 09:46:29 -0700

    Make event handler catch impossible actions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 #!.venv/Scripts/python
 import copy
+import traceback
+
 import tcod
 
 import color
@@ -64,7 +66,14 @@ def main() -> None:
             root_console.clear()
             engine.event_handler.on_render(console=root_console)
             context.present(root_console)
-            engine.event_handler.handle_events(context)
+            try:
+                for event in tcod.event.wait():
+                    context.convert_event(event)
+                    engine.event_handler.handle_events(event)
+            except Exception:   # Handle exceptions in game.
+                traceback.print_exc()   # Print error to stderr
+                # Then print the error to the message log.
+                engine.message_log.add_message(traceback.format_exc(), color.error)
 
 if __name__ == '__main__':
     main()
